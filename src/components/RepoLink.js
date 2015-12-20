234d67c5147537c0691758da1bfae3cb26b3b9dd
meriadec <meriadec.pillet@gmail.com>
2015-12-20 18:13:01 +0100

    feat(core): component

---
+++ src/components/RepoLink.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchAndGo } from 'actions'
+
+@connect()
+class RepoLink extends Component {
+
+  handleClick (e) {
+    e.preventDefault()
+    this.props.dispatch(fetchAndGo(this.props.to))
+  }
+
+  render () {
+    return (
+      <a onClick={::this.handleClick}>
+        {this.props.children}
+      </a>
+    )
+  }
+
+}
+
+export default RepoLink

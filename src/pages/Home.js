a8396c693e90ab0d2914d8fb59fcedb502b4644d
meriadec <meriadec.pillet@gmail.com>
2016-01-17 18:17:10 +0100

    feat(home): better featured repos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import { connect } from 'react-redux'
 import { prefetch } from 'react-fetcher'
 
 import RepoSearch from 'components/RepoSearch'
+import RepoLink from 'components/RepoLink'
 
 import { fetchTrendingRepos, askAndGo } from 'actions/repos'
+
+if (process.env.BROWSER) { require('styles/Featured.scss') }
 
 @prefetch(({ dispatch }) => dispatch(fetchTrendingRepos()))
 @connect(
@@ -25,16 +29,34 @@ class Home extends Component {
       <div>
         <div className='HomeMarged'>
           <h1>{'Dig into Github repo\'s popularity'}</h1>
-          <h2>{'See & analyze stars origin in time and more!'}</h2>
+          <h2 className='mb2'>{'See & analyze stars origin in time and more!'}</h2>
           <RepoSearch autofocus onSelect={::this.goToRepo} />
         </div>
-        <div className='container'>
+        <div className='container mt2'>
 
-          <h1>{'Featured repos'}</h1>
+          <div className='f mb2'>
+            <h2 className='fg'>{'Featured repos'}</h2>
+            <Link to='/browse'>{'Browse all'}</Link>
+          </div>
 
-          {trending.map(repo => (
-            <div key={repo._id}>{repo.name}</div>
-          ))}
+          {trending.length >= 4 && (
+            <div className='Featured'>
+
+              <div>
+                <Feat1 repo={trending[0]} />
+              </div>
+
+              <div>
+                <Feat1 repo={trending[1]} />
+                <Feat1 repo={trending[2]} />
+              </div>
+
+              <div>
+                <Feat2 repo={trending[3]} />
+              </div>
+
+            </div>
+          )}
 
         </div>
       </div>
@@ -42,5 +64,29 @@ class Home extends Component {
   }
 
 }
+
+const FeatTitle = ({ repo }) => {
+  const [author, name] = repo.name.split('/')
+  return (
+    <RepoLink repo={repo} className='feat--title mb02'>
+      {`${author}/`}
+      <strong>{name}</strong>
+    </RepoLink>
+  )
+}
+
+const Feat1 = ({ repo }) => (
+  <div className='feat feat-1'>
+    <FeatTitle repo={repo} />
+    <div className='feat--lang'>{'Javascript'}</div>
+  </div>
+)
+
+const Feat2 = ({ repo }) => (
+  <div className='feat feat-2'>
+    <FeatTitle repo={repo} />
+    <div className='feat--lang'>{'Javascript'}</div>
+  </div>
+)
 
 export default Home

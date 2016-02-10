098b0a1d16ebccc38d595afdb68c9471d747ca81
Aperçu <bgronon@gmail.com>
2016-02-10 14:22:44 +0100

    feat(core): add shot service with imgur upload

---
+++ src/api/Shot.service.js
@@ -0,0 +1,36 @@
+import webshot from 'webshot'
+import imgur from 'imgur'
+import dotenv from 'dotenv'
+
+import config from 'config'
+
+dotenv.load()
+
+imgur.setClientId(process.env.IMGUR_CLIENT)
+imgur.setCredentials('imgur@bangular.io', process.env.IMGUR_PASS, process.env.IMGUR_CLIENT)
+
+export default url => {
+
+  return new Promise((resolve, reject) => {
+
+    const stream = webshot(url, {
+      phantomPath: '/usr/bin/phantomjs',
+      renderDelay: 1e3,
+      errorIfJSException: true
+    })
+
+    const chunks = []
+
+    stream.on('data', chunk => chunks.push(chunk))
+
+    stream.on('end', () => {
+      const data = Buffer.concat(chunks).toString('base64')
+
+      imgur.uploadBase64(data)
+        .then(json => resolve(json.data.link))
+        .catch(err => reject(err.message))
+    })
+
+  })
+
+}

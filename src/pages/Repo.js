c1e4eb5f074deba261cc760584f4e848f73676ef
Aperçu <bgronon@gmail.com>
2016-02-10 12:41:49 +0100

    fix(ux): improve understandability when repo has reached 40k
    Closes #20.

--- src/pages/Repo.js
+++ src/pages/Repo.js
@@ -3,6 +3,7 @@ if (process.env.BROWSER) { require('styles/Repo.scss') }
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { prefetch } from 'react-fetcher'
+import { Link } from 'react-router'
 
 import StarsEvolution from 'components/graphs/StarsEvolution'
 
@@ -27,10 +28,24 @@ class Repo extends Component {
     )
   }
 
+  renderLimitError () {
+    return (
+      <div className='mt3' style={{ color: 'orangered', textAlign: 'center' }}>
+        <p className='mb'>{'Due to Github API limitations, we cannot fetch repo with more than 40.000 stars for the moment.'}</p>
+        <p>
+          <a href='' target='_blank'>Sign the petition here</a>
+          <span className='bullet' />
+          <Link to='/'>{'Bring me back'}</Link>
+        </p>
+      </div>
+    )
+  }
+
   render () {
     const { repo, progress } = this.props
 
     if (!repo) { return this.renderPlaceholder() }
+    if (repo.summary.starsCount > 40000) { return this.renderLimitError() }
 
     return (
       <div className='container mt2'>

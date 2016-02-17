2f3ec8d3adcbe0d40f34b34e28166232e7c7303c
meriadec <meriadec.pillet@gmail.com>
2016-02-18 00:56:45 +0100

    chore(log): omg?

--- src/api/cron.js
+++ src/api/cron.js
@@ -2,6 +2,10 @@ import schedule from 'node-schedule'
 
 import { refreshAll } from 'api/Repo.service'
 
+console.log(`***************************************`)
+console.log(`**         SCHEDULING THINGS         **`)
+console.log(`***************************************`)
+
 schedule.scheduleJob('00 00 00 * * *', () => {
   console.log(`==> STARTING CRON, BRO`)
   refreshAll()

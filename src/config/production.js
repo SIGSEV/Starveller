60c1c617ca83733cf0c64e4b1dde6572ff6a898d
Balthazar Gronon <Apercu@users.noreply.github.com>
2016-02-21 01:35:08 +0100

    chore(config): update new domain url

--- src/config/production.js
+++ src/config/production.js
@@ -5,8 +5,8 @@ export default {
   port: 3044,
   socketPort: 3067,
 
-  clientUrl: 'http://starveller.bangular.io/',
-  apiUrl: 'http://starveller.bangular.io/api',
+  clientUrl: 'http://starveller.sigsev.io/',
+  apiUrl: 'http://starveller.sigsev.io/api',
 
   getApi: () => process.env.BROWSER ? '/api' : 'http://localhost:3044/api'
 
